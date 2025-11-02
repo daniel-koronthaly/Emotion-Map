93f330d5f0be1a71398b032d3a4d9ad11d0efb30
Daniel Koronthály <daniel@koronthaly.net>
2025-11-01 22:21:35 -0700

    added favicon and turned off testing config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ import styles from "./styles/Theme.module.css"
 import ExportToCsv from "./components/body/ExportToCsv";
 import About from "./components/body/About";
 
-const useFakeDataForTestingPurposes = true
+const useFakeDataForTestingPurposes = false
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8000";
 
 function App() {
